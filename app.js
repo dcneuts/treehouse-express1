d4060e4eb8e0e2259004cb529787f2ba0c1d6ec6
Derek Neuts <dcneuts@gmail.com>
2017-07-30 00:27:05 -0700

    Added root for routing and a default response, confirmed working to get started on body of project

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
 
+// root route for app
+app.get('/', (request, response) => {
+	response.send('I love treehouse!');
+});
+
 // setup express dev server
 app.listen(3000);

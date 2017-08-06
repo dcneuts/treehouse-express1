10fc0b3023fd0871b01d3df100a0cd10757e82cd
Derek Neuts <dcneuts@gmail.com>
2017-08-05 23:30:46 -0700

    Added pug(Jade) to the project, using Prepros for testing and exporting html files, switched over app.js to use pug as the rendering template.

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'pug');
+
 // root route for app
-app.get('/', (request, response) => {
-	response.send('I love treehouse!');
+app.get('/', (req, res) => {
+	res.render('index');
+});
+
+app.get('/hello', (req, res) => {
+	res.send('<h1>Hello JavaScript developer!</h1>');
 });
 
 // setup express dev server
-app.listen(3000);
+app.listen(3000, () => {
+	console.log('The application is running on localhost:3000')
+});

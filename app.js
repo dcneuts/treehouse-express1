bd35a03bbf191cf425a9c6304a35a574012554c4
Derek Neuts <dcneuts@gmail.com>
2017-08-23 01:08:19 -0700

    Created logic conditions in Pug to allow random cards to be drawn, along with the ability to start immediately after login

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const express = require ('express');
+const bodyParser = require ('body-parser');
+const cookieParser = require ('cookie-parser');
 
-const app = express();
+const app = express ();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use (bodyParser.urlencoded ({extended: false}));
+app.use (cookieParser ());
 
-app.set('view engine', 'pug');
+app.set ('view engine', 'pug');
 
-const mainRoutes = require('./routes');
-const cardRoutes = require('./routes/cards');
+const mainRoutes = require ('./routes');
+const cardRoutes = require ('./routes/cards');
 
-app.use(mainRoutes);
-app.use('/cards', cardRoutes);
+app.use (mainRoutes);
+app.use ('/cards', cardRoutes);
 
-app.use((req, res, next) => {
-	const err = new Error('Not Found');
-	err.status = 404;
-	next(err);
+app.use ((req, res, next) => {
+    const err = new Error ('Not Found');
+    err.status = 404;
+    next (err);
 });
 
-app.use((err, req, res, next) => {
-	res.locals.error = err;
-	res.status(err.status);
-	res.render('error');
+app.use ((err, req, res, next) => {
+    res.locals.error = err;
+    res.status (err.status);
+    res.render ('error');
 });
 
 // setup express dev server
-app.listen(3000, () => {
-	console.log('The application is running on localhost:3000')
+app.listen (3000, () => {
+    console.log ('The application is running on localhost:3000')
 });

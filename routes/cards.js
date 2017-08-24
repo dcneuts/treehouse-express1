52530d8fa11047d1f744cd032f1fc2270c6dca9d
Derek Neuts <dcneuts@gmail.com>
2017-08-23 23:58:04 -0700

    Backtracking to fix a major problem that was possibly with a pug template

--- routes/cards.js
+++ routes/cards.js
@@ -6,21 +6,16 @@ const {cards} = data;
 router.get ('/', (req, res) => {
     const numberOfCards = cards.length;
     const flashcardId = Math.floor (Math.random () * numberOfCards);
-    res.redirect (`/cards/${flashcardId}`)
+  res.redirect( `/cards/${flashcardId}?side=question` )
 });
 
 router.get ('/:id', (req, res) => {
     const {side} = req.query;
     const {id} = req.params;
-    
-    if(!side) {
-        res.redirect(`/cards/${id}?side=question`);
-    }
-    const name = req.cookies.username;
     const text = cards[id][side];
     const {hint} = cards[id];
     
-    const templateData = {id, text, name};
+    const templateData = {id, text};
     
     if (side === 'question') {
         templateData.hint = hint;
@@ -28,7 +23,7 @@ router.get ('/:id', (req, res) => {
         templateData.sideToShowDisplay = 'Answer';
     } else if (side === 'answer') {
         templateData.sideToShow = 'question';
-        templateData.sideToShowDisplay = "Question";
+      templateData.sideToShowDisplay = 'Question';
     }
     
     res.render ('card', templateData);
